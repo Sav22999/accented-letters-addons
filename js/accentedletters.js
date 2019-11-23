eeabcbb4110f6a739cbacc9f1c19611bfc8dfa9b
Saverio <saverio.morelli@outlook.it>
2019-11-23 22:57:04 +0100

    Updated to 1.1: some improvements
    - Added support to "Firefox theme" (dark/light): the main icon changed
    - Improvements at the GUI: now it's similar to Firefox Design
    - Improvements to JS file
    - New main icon
    - New main font-family: Source Code Pro (letters) and Zilla Slab (titles of sections)

--- js/accentedletters.js
+++ js/accentedletters.js
@@ -10,35 +10,36 @@ function copyLetter(text)
 	document.getElementById("text_to_copy").style.display="none";
 }
 
-function setLowercase()
+function setLowerUpperCase(type)
 {
 	for(var i=0;i<n_letters;i++)
 	{
-		document.getElementsByClassName("letter")[i].value = (document.getElementsByClassName("letter")[i].value).toLowerCase();
+		type == "upper" ? document.getElementsByClassName("letter")[i].value = (document.getElementsByClassName("letter")[i].value).toUpperCase() : document.getElementsByClassName("letter")[i].value = (document.getElementsByClassName("letter")[i].value).toLowerCase();
 
 		//document.getElementsByClassName("letter")[i].onclick = function(e){copyLetter(i);};
 	}
-	document.getElementById("lowercase").style.borderBottomColor="black";
-	document.getElementById("uppercase").style.borderBottomColor="transparent";
-}
-
-function setUppercase()
-{
-	for(var i=0;i<n_letters;i++)
+	if (type == "upper")
 	{
-		document.getElementsByClassName("letter")[i].value = (document.getElementsByClassName("letter")[i].value).toUpperCase();
-
-		//document.getElementsByClassName("letter")[i].onclick = function(e){copyLetter(i);};
+		document.getElementById("lowercase").style.borderTopColor="transparent";
+		document.getElementById("lowercase").style.color="black";
+		document.getElementById("uppercase").style.borderTopColor="rgb(10, 132, 255)";
+		document.getElementById("uppercase").style.color="rgb(10, 132, 255)";
 	}
-	document.getElementById("lowercase").style.borderBottomColor="transparent";
-	document.getElementById("uppercase").style.borderBottomColor="black";
+	else if (type == "lower")
+	{
+		document.getElementById("lowercase").style.borderTopColor="rgb(10, 132, 255)";
+		document.getElementById("lowercase").style.color="rgb(10, 132, 255)";
+		document.getElementById("uppercase").style.borderTopColor="transparent";
+		document.getElementById("uppercase").style.color="black";
+	}
+
 }
 
-setLowercase();
+setLowerUpperCase("lower");
 
 // button click events
-document.getElementById("lowercase").onclick = function(e){setLowercase();};
-document.getElementById("uppercase").onclick = function(e){setUppercase();};
+document.getElementById("lowercase").onclick = function(e){setLowerUpperCase("lower");};
+document.getElementById("uppercase").onclick = function(e){setLowerUpperCase("upper");};
 
 for(var i=0;i<n_letters;i++)
 {
